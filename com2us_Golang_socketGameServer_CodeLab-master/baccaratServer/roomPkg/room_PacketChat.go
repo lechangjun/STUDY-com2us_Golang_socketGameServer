4bd383ef9ccd5e947c87f1e50b0bd6d1992e7595
lechangjun <68671394+lechangjun@users.noreply.github.com>
2021-07-24 01:28:33 -0700

    Update room_PacketChat.go

--- com2us_Golang_socketGameServer_CodeLab-master/baccaratServer/roomPkg/room_PacketChat.go
+++ com2us_Golang_socketGameServer_CodeLab-master/baccaratServer/roomPkg/room_PacketChat.go
@@ -1,25 +1,29 @@
 package roomPkg
 
-import (
+import 
+(
 	. "gohipernetFake"
 
 	"main/protocol"
 )
 
 
-func (room *baseRoom) _packetProcess_Chat(user *roomUser, packet protocol.Packet) int16 {
+func (room *baseRoom) _packetProcess_Chat(user *roomUser, packet protocol.Packet) int16 
+{
 	sessionIndex := packet.UserSessionIndex
 	sessionUniqueId := packet.UserSessionUniqueId
 
 	var chatPacket protocol.RoomChatReqPacket
-	if chatPacket.Decoding(packet.Data) == false {
+	if chatPacket.Decoding(packet.Data) == false 
+	{
 		_sendRoomChatResult(sessionIndex, sessionUniqueId, protocol.ERROR_CODE_PACKET_DECODING_FAIL)
 		return protocol.ERROR_CODE_PACKET_DECODING_FAIL
 	}
 
 	// 채팅 최대길이 제한
 	msgLen := len(chatPacket.Msgs)
-	if msgLen < 1 || msgLen > protocol.MAX_CHAT_MESSAGE_BYTE_LENGTH {
+	if msgLen < 1 || msgLen > protocol.MAX_CHAT_MESSAGE_BYTE_LENGTH 
+	{
 		_sendRoomChatResult(sessionIndex, sessionUniqueId, protocol.ERROR_CODE_ROOM_CHAT_CHAT_MSG_LEN)
 		return protocol.ERROR_CODE_ROOM_CHAT_CHAT_MSG_LEN
 	}
@@ -38,7 +42,8 @@ func (room *baseRoom) _packetProcess_Chat(user *roomUser, packet protocol.Packet
 	return protocol.ERROR_CODE_NONE
 }
 
-func _sendRoomChatResult(sessionIndex int32, sessionUniqueId uint64, result int16) {
+func _sendRoomChatResult(sessionIndex int32, sessionUniqueId uint64, result int16) 
+{
 	response := protocol.RoomChatResPacket{ result }
 	sendPacket, _ := response.EncodingPacket()
 	NetLibIPostSendToClient(sessionIndex, sessionUniqueId, sendPacket)
