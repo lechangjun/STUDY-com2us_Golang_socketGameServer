4832fc877fb484a325ffe21ece91acffe3a96699
lechangjun <68671394+lechangjun@users.noreply.github.com>
2021-07-24 01:27:53 -0700

    Update room_Packet.go

--- com2us_Golang_socketGameServer_CodeLab-master/baccaratServer/roomPkg/room_Packet.go
+++ com2us_Golang_socketGameServer_CodeLab-master/baccaratServer/roomPkg/room_Packet.go
@@ -1,10 +1,12 @@
 package roomPkg
 
-import (
+import 
+(
 	"main/protocol"
 )
 
-func (room *baseRoom) _packetProcess_Relay(user *roomUser, packet protocol.Packet) int16 {
+func (room *baseRoom) _packetProcess_Relay(user *roomUser, packet protocol.Packet) int16 
+{
 	var relayNotify protocol.RoomRelayNtfPacket
 	relayNotify.RoomUserUniqueId = user.RoomUniqueId
 	relayNotify.Data = packet.Data
