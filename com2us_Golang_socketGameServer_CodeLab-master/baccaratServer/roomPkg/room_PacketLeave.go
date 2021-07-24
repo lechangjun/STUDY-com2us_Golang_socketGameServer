abbe4b7c6ecf5d4092f74c6ca6b167867349edfb
lechangjun <68671394+lechangjun@users.noreply.github.com>
2021-07-24 01:32:00 -0700

    Update room_PacketLeave.go

--- com2us_Golang_socketGameServer_CodeLab-master/baccaratServer/roomPkg/room_PacketLeave.go
+++ com2us_Golang_socketGameServer_CodeLab-master/baccaratServer/roomPkg/room_PacketLeave.go
@@ -1,6 +1,7 @@
 package roomPkg
 
-import (
+import 
+(
 	"time"
 
 	. "gohipernetFake"
@@ -10,7 +11,8 @@ import (
 )
 
 
-func (room *baseRoom) _packetProcess_LeaveUser(user *roomUser, packet protocol.Packet) int16 {
+func (room *baseRoom) _packetProcess_LeaveUser(user *roomUser, packet protocol.Packet) int16 
+{
 	room._leaveUserProcess(user)
 
 	sessionIndex := packet.UserSessionIndex
@@ -19,7 +21,8 @@ func (room *baseRoom) _packetProcess_LeaveUser(user *roomUser, packet protocol.P
 	return protocol.ERROR_CODE_NONE
 }
 
-func (room *baseRoom) _leaveUserProcess(user *roomUser) {
+func (room *baseRoom) _leaveUserProcess(user *roomUser) 
+{
 	//TODO 방의 상태가 ROOM_STATE_NOE, ROOM_STATE_GAME_RESULT 일 때만 나갈 수 있다.
 
 	//TODO 유저가 접속이 끊어져서 나가는 경우라면 게임이 끝날 때까지 유저 정보 들고 있다가
@@ -38,13 +41,15 @@ func (room *baseRoom) _leaveUserProcess(user *roomUser) {
 }
 
 
-func _sendRoomLeaveResult(sessionIndex int32, sessionUniqueId uint64, result int16) {
+func _sendRoomLeaveResult(sessionIndex int32, sessionUniqueId uint64, result int16) 
+{
 	response := protocol.RoomLeaveResPacket{ result }
 	sendPacket, _ := response.EncodingPacket()
 	NetLibIPostSendToClient(sessionIndex, sessionUniqueId, sendPacket)
 }
 
-func (room *baseRoom) _sendRoomLeaveUserNotify(roomUserUniqueId uint64, userSessionUniqueId uint64) {
+func (room *baseRoom) _sendRoomLeaveUserNotify(roomUserUniqueId uint64, userSessionUniqueId uint64) 
+{
 	notifyPacket := protocol.RoomLeaveUserNtfPacket{roomUserUniqueId	}
 	sendBuf, packetSize := notifyPacket.EncodingPacket()
 	room.broadcastPacket(int16(packetSize), sendBuf, userSessionUniqueId)
