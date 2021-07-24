6057ed7381df6b7d8b35aa9410168970f08f6b66
lechangjun <68671394+lechangjun@users.noreply.github.com>
2021-07-24 01:25:30 -0700

    Update define.go

--- com2us_Golang_socketGameServer_CodeLab-master/baccaratServer/roomPkg/define.go
+++ com2us_Golang_socketGameServer_CodeLab-master/baccaratServer/roomPkg/define.go
@@ -1,13 +1,15 @@
 package roomPkg
 
-type RoomConfig struct {
+type RoomConfig struct 
+{
 	StartRoomNumber int32
 	MaxRoomCount int32
 	MaxUserCount int32
 }
 
 
-type addRoomUserInfo struct {
+type addRoomUserInfo struct 
+{
 	userID []byte
 
 	netSessionIndex     int32
@@ -15,7 +17,8 @@ type addRoomUserInfo struct {
 }
 
 // 방의 상태
-const (
+const 
+(
 	ROOM_STATE_NOE = 1
 	ROOM_STATE_GAME_WAIT_BATTING = 2
 	ROOM_STATE_GAME_RESULT = 3
@@ -25,9 +28,11 @@ const (
 const MAX_CARD_CONT = 52
 const CARD_ROW_COUNT = 13
 // 카드 순서 스페이드, 다이아몬드, 클로버, 하트 A,2,3,4,5,6,7,8,9,10,J,Q,K
-func makeCard() []int8 {
+func makeCard() []int8 
+{
 	a := make([]int8, MAX_CARD_CONT)
-	for i := range a {
+	for i := range a 
+	{
 		a[i] = (int8)(i)
 	}
 	return a
@@ -36,20 +41,23 @@ func makeCard() []int8 {
 const BATTING_WAIT_MILLISEC = 5000
 const NEXT_GAME_WAIT_MILLISEC = 10000
 
-const (
+const 
+(
 	BATTING_SELECT_NONE = 0
 	BATTING_SELECT_PLAYER = 1
 	BATTING_SELECT_BANKER = 2
 )
 
 // 게임 결과
-const (
+const 
+(
 	GAME_RESULT_WIN_PLAYER = 1
 	GAME_RESULT_WIN_BANKER = 2
 	GAME_RESULT_TIE = 3
 )
 
-type baccaratGameResultInfo struct {
+type baccaratGameResultInfo struct 
+{
 	cardsBanker [3]int8
 	cardsPlayer [3]int8
 
